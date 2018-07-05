58b89f4b2afae297dce82762d26ec3f7810b1e9f
Monil Saraswat <monilsaraswat@gmail.com>
2018-07-05 15:58:31 +0530

    Initial commit

---
+++ app/src/main/java/com/example/hp15/camera/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.hp15.camera;
+
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int CAMERA_REQUEST = 1888;
+    ImageView imageView;
+    Button photoButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imageView = (ImageView)findViewById(R.id.imgView);
+        photoButton = (Button)findViewById(R.id.btnPhoto);
+
+        photoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+                startActivityForResult(cameraIntent,CAMERA_REQUEST);
+            }
+        });
+    }
+    protected void onActivityResult(int requestCode,int resultCode,Intent data)
+    {
+        if (requestCode == CAMERA_REQUEST) {
+            Bitmap photo = (Bitmap) data.getExtras().get("data");
+            imageView.setImageBitmap(photo);
+        }
+           }
+}
